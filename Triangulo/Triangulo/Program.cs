ed7753b84b48b32fbcc4c1130f9786d6427ce67d
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-08-11 14:59:01 -0300

    Uri 1043 Triangulo

---
+++ Triangulo/Triangulo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, area, perimetro;
+
+            string[] valores = Console.ReadLine().Split(' ');
+            a = double.Parse(valores[0],CultureInfo.InvariantCulture);
+            b = double.Parse(valores[1],CultureInfo.InvariantCulture);
+            c = double.Parse(valores[2],CultureInfo.InvariantCulture);
+
+            if ( a < b + c && b < a + c && c < a + b)
+            {
+                perimetro = a + b + c;
+                Console.WriteLine("Perimetro = " + perimetro.ToString("F1", CultureInfo.InvariantCulture));
+            }
+            else
+            {
+                area = ((a + b) * c) / 2;
+                Console.WriteLine("Area = " + area.ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
